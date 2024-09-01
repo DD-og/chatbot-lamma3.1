e44d69d5353d19e1a8f0b7bc174ca92526ccf25c
Dev Mehta <dev231755@gmail.com>
2024-09-01 20:11:57 +0530

    first

--- main.py
+++ main.py
@@ -1,51 +1,63 @@
 import os
+import json
+
 import streamlit as st
 from groq import Groq
 
-# Streamlit page configuration
+
+# streamlit page configuration
 st.set_page_config(
-    page_title="IPR Chatbot",
-    page_icon="🚀",
+    page_title="LLAMA 3.1. Chat",
+    page_icon="🦙",
     layout="centered"
 )
 
-# Get API key from environment variable
-GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+working_dir = os.path.dirname(os.path.abspath(__file__))
+config_data = json.load(open(f"{working_dir}/config.json"))
 
-# Initialize Groq client
-client = Groq(api_key=GROQ_API_KEY)
+GROQ_API_KEY = config_data["GROQ_API_KEY"]
 
-# Initialize the chat history as streamlit session state if not present already
+# save the api key to environment variable
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+client = Groq()
+
+# initialize the chat history as streamlit session state of not present already
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
-# Streamlit page title
-st.title("🚀 IPR ChatBot")
 
-# Display chat history
+# streamlit page title
+st.title("🦙 LLAMA 3.1. ChatBot")
+
+# display chat history
 for message in st.session_state.chat_history:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Input field for user's message
-user_prompt = st.chat_input("Ask bot...")
+
+# input field for user's message:
+user_prompt = st.chat_input("Ask LLAMA...")
 
 if user_prompt:
+
     st.chat_message("user").markdown(user_prompt)
     st.session_state.chat_history.append({"role": "user", "content": user_prompt})
 
-    # Send user's message to the LLM and get a response
+    # sens user's message to the LLM and get a response
     messages = [
         {"role": "system", "content": "You are a helpful assistant"},
         *st.session_state.chat_history
     ]
+
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
         messages=messages
     )
+
     assistant_response = response.choices[0].message.content
     st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
 
-    # Display the LLM's response
+    # display the LLM's response
     with st.chat_message("assistant"):
         st.markdown(assistant_response)
